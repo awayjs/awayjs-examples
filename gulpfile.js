206a58c042d4f6d188e2510bb636d3945cc82c8c
rob-bateman <rob@infiniteturtles.co.uk>
2015-05-11 14:35:46 +0100

    restore gulpfile package back to version 0.7.8

--- gulpfile.js
+++ gulpfile.js
@@ -6,47 +6,41 @@ var source = require('vinyl-source-stream');
 var transform = require('vinyl-transform');
 var exorcist = require('exorcist');
 var sourcemaps = require('gulp-sourcemaps');
-var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 var argv = require('yargs').argv;
-var typescript = require('gulp-typescript');
 var watchify = require('watchify');
 var package = require('./package.json');
 var livereload = require('gulp-livereload');
 var tsify = require('tsify');
-
-function _compile(filename, callback) {
-    console.log('compile ' + filename);
-    var tsProject = typescript.createProject({
-        declarationFiles: true,
-        noExternalResolve: true,
-        target: 'ES5',
-        module: 'commonjs',
-        sourceRoot: './src'
-    });
-
-    var tsResult = gulp.src(['./src/' + filename + '.ts', './node_modules/awayjs-**/build/*.d.ts'])
-        .pipe(sourcemaps.init())
-        .pipe(typescript(tsProject));
-
-    tsResult.js
-        .pipe(sourcemaps.write({sourceRoot: './'}))
-        .pipe(gulp.dest('./src'))
-        .on('end', callback);
-}
+var watch;
 
 function _package(filename, callback) {
     console.log('package ' + filename);
     var b = browserify({
         debug: true,
-        entries: './src/' + filename + '.js'
+        entries: './src/' + filename + '.ts',
+        cache: {},
+        packageCache: {},
+        fullPaths: true
     });
 
-    glob('./node_modules/awayjs-**/lib/**/*.js', {}, function (error, files) {
+    b.plugin('tsify', {target:'ES5', sourceRoot:'./', noExternalResolve: true, declarationFiles: './node_modules/awayjs-**/build/*.d.ts'});
 
+    glob('./node_modules/awayjs-**/lib/**/*.ts', {}, function (error, files) {
         files.forEach(function (file) {
-            b.external(file, {expose:unixStylePath(path.relative('./node_modules/', file.slice(0,-3)))});
+            b.external(file);
         });
+
+        if (watch) {
+            b = watchify(b);
+            b.on('update', function(){
+                return b.bundle()
+                    .pipe(source(filename + '.js'))
+                    .pipe(gulp.dest('./bin/js'))
+                    .pipe(livereload());
+            });
+        }
 
         b.bundle()
             .pipe(exorcist('./bin/js/' + filename + '.js.map'))
@@ -68,16 +62,18 @@ function _minify(filename) {
         .pipe(gulp.dest('./bin/js/'))
 }
 
-gulp.task('compile', function(callback) {
-    _compile(argv.file, callback);
+gulp.task('package', function(callback){
+    watch = false;
+    _package(argv.main, callback);
 });
 
-gulp.task('package', ['compile'], function(callback){
-    _package(argv.file, callback);
+gulp.task('package-watch', function(callback){
+    watch = true;
+    _package(argv.main, callback);
 });
 
 gulp.task('package-min', ['package'], function(){
-    return _minify(argv.file);
+    return _minify(argv.main);
 });
 
 gulp.task('package-all', function(){
@@ -86,96 +82,35 @@ gulp.task('package-all', function(){
         return files.forEach(function (file, index) {
             var filename = path.basename(file, '.ts');
 
-            _compile(filename, function() {
-                    _package(filename, function() {
-                        _minify(filename);
-                    });
-                });
+            _package(filename, function() {
+                _minify(filename);
+            });
         });
     });
 });
 
-
-gulp.task('package-awayjs', function(callback){
-    var b = browserify({
-        debug: true,
-        paths: ['../']
-    });
-
-    glob('./node_modules/awayjs-**/lib/**/*.js', {}, function (error, files) {
-
-        files.forEach(function (file) {
-            b.require(file, {expose:unixStylePath(path.relative('./node_modules/', file.slice(0,-3)))});
-        });
-
-        b.bundle()
-            .pipe(exorcist('./bin/js/awayjs-dist-require.js.map'))
-            .pipe(source('awayjs-dist-require.js'))
-            .pipe(gulp.dest('./bin/js'))
-            .on('end', callback);
-    });
-});
-
-gulp.task('package-awayjs-min', ['package-awayjs'], function(callback){
-    return gulp.src('./bin/js/awayjs-dist-require.js')
-        .pipe(sourcemaps.init({loadMaps:true}))
-        .pipe(uglify({compress:false}))
-        .pipe(sourcemaps.write())
-        .pipe(transform(function() {
-            return exorcist('./bin/js/awayjs-dist-require.js.map');
-        }))
-        .pipe(gulp.dest('./bin/js'));
-});
-
-gulp.task('watch', ['package-awayjs-watch'], function(){
+gulp.task('watch', ['package-awayjs', 'package-watch'], function() {
+    gulp.watch('./node_modules/awayjs-**/build/**/*.js', ['package-awayjs']);
 
     //Start live reload server
     livereload.listen();
 });
 
-gulp.task('package-awayjs-watch', function(callback){
-    var b = browserify({
-        debug: true,
-        paths: ['../'],
-        cache:{},
-        packageCache:{},
-        fullPaths:true
-    });
-
-    glob('./node_modules/awayjs-**/lib/**/*.js', {}, function (error, files) {
-
-        files.forEach(function (file) {
-            b.require(file, {expose:unixStylePath(path.relative('./node_modules/', file.slice(0,-3)))});
-        });
-
-        b = watchify(b);
-        b.on('update', function(){
-            bundleShare(b, 'awayjs-dist-require.js');
-        });
-
-        bundleShare(b, 'awayjs-dist-require.js')
-            .on('end', callback);
-    })
-});
-
-
-gulp.task('watch-fast', ['package-awayjs-fast'], function() {
-    gulp.watch('./node_modules/awayjs-**/build/**/*.js', ['package-awayjs-fast']);
-});
-
-gulp.task('package-awayjs-fast', function(){
+gulp.task('package-awayjs', function(){
     //extract awayjs dependencies from package.json
     var awayjsDependencies = [];
     Object.keys(package.dependencies).forEach(function (key) {
         awayjsDependencies.push('./node_modules/' + key + '/build/' + key + ((argv.min)? '.min.js' : '.js'));
-    })
+    });
 
     if (argv.maps)
         return gulp.src(awayjsDependencies)
             .pipe(sourcemaps.init({loadMaps:true}))
             .pipe(concat('awayjs-dist-require.js'))
             .pipe(sourcemaps.write({sourceRoot:'./'}))
-            .pipe(transform(function() { return exorcist('./bin/js/awayjs-dist-require.js.map'); }))
+            .pipe(transform(function() {
+                return exorcist('./bin/js/awayjs-dist-require.js.map');
+            }))
             .pipe(gulp.dest('./bin/js/'));
 
     return gulp.src(awayjsDependencies)
@@ -183,28 +118,6 @@ gulp.task('package-awayjs-fast', function(){
         .pipe(gulp.dest('./bin/js/'));
 });
 
-
-//
-//
-//gulp.task('package-tsify', function(){
-//    console.log('package ' + argv.file);
-//    var b = browserify({debug: true})
-//        .add('./src/' + argv.file + '.ts')
-//        .plugin('tsify', { target: 'ES5' });
-//
-//    return b.bundle()
-//        //.pipe(unpathify())
-//        .pipe(exorcist('./bin/js/' + argv.file + '.js.map'))
-//        .pipe(source(argv.file + '.js'))
-//        .pipe(gulp.dest('./bin/js/'));
-//});
-
-function bundleShare(b, file) {
-    return b.bundle()
-        .pipe(source(file))
-        .pipe(gulp.dest('./bin/js'))
-        .pipe(livereload());
-}
 
 function unixStylePath(filePath) {
     return filePath.split(path.sep).join('/');
