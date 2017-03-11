6325785f84545ab68191da60ed337641efaf59bf
Alejandro Santander <palebluedot@gmail.com>
2017-03-11 10:38:08 -0300

    Move travis deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,6 +12,13 @@ function doCompile {
     echo "Compiling..."
     npm run build:prod
 }
+
+echo "Generating examples..."
+echo "pwd"
+pwd
+echo "ls -la"
+ls -la
+
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
@@ -56,7 +63,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../scripts/deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
