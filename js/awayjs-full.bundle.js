48c6c560d28f261e648b58054f0ef9bd25356122
Travis CI <palebluedot@gmail.com>
2017-08-02 17:16:07 +0000

    Deploy to GitHub Pages: 985f862487caa3574780434a9758461c7b688170

--- js/awayjs-full.bundle.js
+++ js/awayjs-full.bundle.js
@@ -12992,10 +12992,56 @@ See the Apache Version 2.0 License for specific language governing permissions
 and limitations under the License.
 ***************************************************************************** */
 /* global Reflect, Promise */
+
+var extendStatics = Object.setPrototypeOf ||
+    ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+    function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+
 function __extends(d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    extendStatics(d, b);
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function __read(o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+}
+
+
+
+function __await(v) {
+    return this instanceof __await ? (this.v = v, this) : new __await(v);
 }
 
 /**
@@ -16363,7 +16409,7 @@ var GraphicsPath = (function () {
                     case GraphicsPathCommand.CURVE_TO:
                         end_point = new _awayjs_core.Point(data[data_cnt++], data[data_cnt++]);
                         ctr_point = new _awayjs_core.Point(data[data_cnt++], data[data_cnt++]);
-                        console.log("CURVE_TO ", i, ctr_point.x, ctr_point.y, end_point.x, end_point.y);
+                        //console.log("CURVE_TO ", i, ctr_point.x, ctr_point.y, end_point.x, end_point.y);
                         var curve_verts = [];
                         GraphicsFactoryHelper.tesselateCurve(prev_x, prev_y, ctr_point.x, ctr_point.y, end_point.x, end_point.y, curve_verts);
                         var k_len = curve_verts.length;
